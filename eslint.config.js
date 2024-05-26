8210ea2bf091cfaee28975968e86ddab6fb97742
Aidan Sun <aidansun05@gmail.com>
2024-05-25 22:57:03 -0400

    [skip ci] Update ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginVue from "eslint-plugin-vue";
+import tseslint from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginVue.configs["flat/recommended"],
+  eslintConfigPrettier,
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        sourceType: "module",
+        parser: tseslint.parser
+      },
+      // For Vue Macros:
+      globals: {
+        $$: "readonly",
+        $: "readonly",
+        $computed: "readonly",
+        $customRef: "readonly",
+        $ref: "readonly",
+        $shallowRef: "readonly",
+        $toRef: "readonly"
+      }
+    }
+  },
+  {
+    rules: {
+      quotes: ["warn", "double", { avoidEscape: true }]
+    }
+  }
+);
