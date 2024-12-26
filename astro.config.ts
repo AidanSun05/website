c50f2c31ffba942e87aa46986e1922a3696ef2de
Aidan Sun <aidansun05@gmail.com>
2024-12-26 16:10:17 -0500

    Remove Astro integrations

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,11 @@ import markdoc from "@astrojs/markdoc";
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
-import Macros from "@vue-macros/astro";
-import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.aidansun.com",
-  integrations: [markdoc(), vue(), Macros(), sitemap(), robotsTxt()],
-  output: "hybrid",
+  integrations: [asciidoc(), vue(), sitemap()],
   adapter: node({ mode: "standalone" })
 });
