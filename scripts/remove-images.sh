152c2426b7f8871a8a1a063f6bf03b81d557fcc0
Aidan Sun <aidansun05@gmail.com>
2024-12-27 18:38:10 -0500

    Simplify script

--- scripts/remove-images.sh
+++ scripts/remove-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-for IMAGE in "server"
-do
-  if [ ! -z "$(doctl registry repository list | grep "$(echo $IMAGE)")" ]
-  then
-    doctl registry repository dm $(echo $IMAGE) $(doctl registry repository lt $(echo $IMAGE) | grep -o "sha.*") --force
-  fi
-done
+IMAGE=server
+if [ ! -z "$(doctl registry repository list | grep "$(echo $IMAGE)")" ]; then
+  doctl registry repository dm $(echo $IMAGE) $(doctl registry repository lt $(echo $IMAGE) | grep -o "sha.*") --force
+fi
