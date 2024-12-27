d7aafc0d2eadba9fedde87f7adebb129d76def82
Aidan Sun <aidansun05@gmail.com>
2024-12-27 18:52:33 -0500

    Format code

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
-import asciidoc from "./src/asciidoc";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+import asciidoc from "./src/asciidoc";
 
 // https://astro.build/config
 export default defineConfig({
