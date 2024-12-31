bd211797f0e272bc93c63cfd0fca5d22cc22fe3f
Aidan Sun <aidansun05@gmail.com>
2024-12-30 22:02:26 -0500

    Remove unused file

--- src/common/store/tabs.ts
+++
@@ -1,7 +0,0 @@
-import { map } from "nanostores";
-
-export const $tabs = map<Record<string, number>>();
-
-export function setTab(id: string, currentTab: number) {
-  $tabs.setKey(id, currentTab);
-}
