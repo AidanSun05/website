d030dfc6be1424a4ccffb8350ad87c9c9c6d4991
Aidan Sun <aidansun05@gmail.com>
2025-02-10 08:00:44 -0500

    Remove unused files

--- scripts/remove-images.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-IMAGE=server
-if [ ! -z "$(doctl registry repository list | grep "$(echo $IMAGE)")" ]; then
-  doctl registry repository dm $(echo $IMAGE) $(doctl registry repository lt $(echo $IMAGE) | grep -o "sha.*") --force
-fi
