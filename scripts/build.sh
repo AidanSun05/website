d9ccfbb81e79edaef7518437ce766f77de575b99
Aidan Sun <aidansun05@gmail.com>
2025-02-12 10:35:28 -0500

    Remove shell script

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd res/lib
-for SUBDIR in *; do zip -r -q "../../public/dl/${SUBDIR}.zip" "$SUBDIR"; done
-
-cd ../..
-npm run build
-npx svgo -f dist/_astro
-node scripts/clean-assets.js
